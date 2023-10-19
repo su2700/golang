f1cb13c5ef19f7de4a1a0319ff658d8fb56cbdd6
N0ah <lhllfc@gmail.com>
2023-10-19 15:17:33 -0400

    add escape char

---
+++ src/go_code/chaper02/escapeChar/main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("tom\tjack")
+	fmt.Println("TOM\nJACK")
+	
+}
---
+++ src/go_code/project1/main/hello.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("Hello World")
+	var num=10
+	fmt.Println(num)
+
+}
+
+
+// hello.go belong to the package main
+// func main, main is name of func, 
+// in go, all file must belong to a package
+//in go, it run line one by one, and semicolon is unnecessary 
+//escape char
+// \t  table, \r return
+
+
+
